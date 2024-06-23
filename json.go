a6c14381f3b4cf8c9ffcd63c45bf5cfb8b2214fe
Vaibhav <vaibhavb1610@gmail.com>
2024-06-24 01:27:59 +0530

    final

--- json.go
+++ json.go
@@ -6,23 +6,6 @@ import (
 	"net/http"
 )
 
-
-
-func respondWithJSON(w http.ResponseWriter,code int,payload interface{}){
-	dat,err := json.Marshal(payload)
-	if err!=nil{
-	log.Println("Failed to marshal JSON response: %v",payload)
-	w.WriteHeader(500)
-	return
-}
-
-
-
-w.Header().Add("Content-Type","application/json")
-w.WriteHeader(code)
-w.Write(dat)
-}
-
 func respondWithError(w http.ResponseWriter, code int, msg string) {
 	if code > 499 {
 		log.Printf("Responding with 5XX error: %s", msg)
@@ -33,4 +16,16 @@ func respondWithError(w http.ResponseWriter, code int, msg string) {
 	respondWithJSON(w, code, errorResponse{
 		Error: msg,
 	})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(code)
+	w.Write(dat)
 }
--- main.go
+++ main.go
@@ -1,26 +1,49 @@
 package main
 
 import (
-	"fmt"
+
+	"database/sql"
 	"log"
 	"os"
+	"time"
 	"github.com/joho/godotenv"
 	"github.com/go-chi/chi/v5"
 	"net/http"
 	"github.com/go-chi/cors"
+	"github.com/vaib1610/rssproj/internal/database"
+	_ "github.com/lib/pq"
+
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main(){
 
-	godotenv.Load()
+	godotenv.Load(".env")
 
-	portString := os.Getenv("PORT")
-	if portString == ""{
-		log.Fatal("PORT is not found in the environment")
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT environment variable is not set")
 	}
-	router:=chi.NewRouter()
 
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		log.Fatal("DATABASE_URL environment variable is not set")
+	}
 
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dbQueries := database.New(db)
+
+	apiCfg := apiConfig{
+		DB: dbQueries,
+	}
+
+	router := chi.NewRouter()
 
 	router.Use(cors.Handler(cors.Options{
 		AllowedOrigins:   []string{"https://*", "http://*"},
@@ -31,27 +54,33 @@ func main(){
 		MaxAge:           300,
 	}))
 
-
 	v1Router := chi.NewRouter()
+
+	v1Router.Post("/users", apiCfg.handlerUsersCreate)
+	v1Router.Get("/users", apiCfg.middlewareAuth(apiCfg.handlerUsersGet))
+
+	v1Router.Post("/feeds", apiCfg.middlewareAuth(apiCfg.handlerFeedCreate))
+	v1Router.Get("/feeds", apiCfg.handlerGetFeeds)
+
+	v1Router.Get("/feed_follows", apiCfg.middlewareAuth(apiCfg.handlerFeedFollowsGet))
+	v1Router.Post("/feed_follows", apiCfg.middlewareAuth(apiCfg.handlerFeedFollowCreate))
+	v1Router.Delete("/feed_follows/{feedFollowID}", apiCfg.middlewareAuth(apiCfg.handlerFeedFollowDelete))
+
+	v1Router.Get("/posts", apiCfg.middlewareAuth(apiCfg.handlerPostsGet))
+
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerErr)
 
 	router.Mount("/v1", v1Router)
-
-
-
-
-
 	srv := &http.Server{
+		Addr:    ":" + port,
 		Handler: router,
-		Addr: ":"+ portString,
 	}
-	
-log.Printf("Server starting on port %v", portString)
 
-	err := srv.ListenAndServe()
-	if err!= nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Port:", portString)
+	const collectionConcurrency = 10
+	const collectionInterval = time.Minute
+	go startScraping(dbQueries, collectionConcurrency, collectionInterval)
+
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
 }
